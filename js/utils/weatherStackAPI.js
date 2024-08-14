f2ccf8fd0a45c9192e88f6e29973bdb7038cbc25
Yhumi0210 <buillaura0210@gmail.com>
2024-08-14 17:57:26 +0200

    bug

--- js/utils/weatherStackAPI.js
+++ js/utils/weatherStackAPI.js
@@ -1,4 +1,4 @@
-const ACCESS_KEY = ''
+const ACCESS_KEY = '8312cf4e11dea76afc0bce64a28744e8'
 
 const _retrieveWeatherForecastMockedData = () => fetch('/data/weather-api-mocked-data.json')
 .then(res => res.json())
