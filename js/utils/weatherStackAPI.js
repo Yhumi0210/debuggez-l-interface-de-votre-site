c9927d7e9176cf3c2443172d9ee4b954ed0bbe3a
Yhumi0210 <buillaura0210@gmail.com>
2024-08-18 17:01:37 +0200

    de bug

--- js/utils/weatherStackAPI.js
+++ js/utils/weatherStackAPI.js
@@ -1,11 +1,11 @@
-const ACCESS_KEY = ''
+const ACCESS_KEY = '8312cf4e11dea76afc0bce64a28744e8'
 
 const _retrieveWeatherForecastMockedData = () => fetch('/data/weather-api-mocked-data.json')
 .then(res => res.json())
 .catch(err => console.log("Oh no", err))
 
 
-const _retrieveWeatherForecastApiData = coordinates => fetch(`https://api.weatherstack.com/current?access_key=${ACCESS_KEY}&query=${coordinates.lat},${coordinates.lng}`)
+const _retrieveWeatherForecastApiData = coordinates => fetch(`https://api.weatherstack.com/current?access_key=${ACCESS_KEY}&query=${coordinates.latitude},${coordinates.longitude}`)
     .then(res => res.json())
     .catch(err => {})
 
